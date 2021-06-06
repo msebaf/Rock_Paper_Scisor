6b615da682731df3c5ca0ef1de10e059b68edb7a
msebaf <msebaf@hotmail.com>
2021-06-05 21:38:33 -0300

    .

--- R_P_S.js
+++ R_P_S.js
@@ -1,47 +1,55 @@
 //La computadora elige
 function computerPlay(){
-    var computerChoise = 0;
-    var min = 1;
-    var max = 3;
-    var x = Math.floor(Math.random()*(max-min+1)+min);
+    let computerChoise = "";
+    let min = 1;
+    let max = 3;
+    let x = Math.floor(Math.random()*(max-min+1)+min);
+    switch (x){
+        case (x=1):
+        computerChoise="rock"
+        break;
+        case (x=2):
+        computerChoise="paper"
+        break;
+        case (x=3):
+        computerChoise="scisor"
+        break;
 
+    }
+    
     return (computerChoise);
+    
     
     
 }
 
 // el usuario elige
 function userPlay () {
-    let choice = 0;
     
-    player_Selection = prompt ("Rock, Paper or Scisor").toLowerCase();
-        
-    switch (player_Selection){
-        case "rock":
-            choice=1;
-            break;
-        case "paper" :
-            choice=2;
-            break;
-        case "scisor":
-            choice=3;
-            break;
+    
+    player_Selection = prompt ("Rock, Paper or Scisor").toLowerCase()
 
-    }
-    return (choice);
+    
+    return (player_Selection);
     
 }
 
+
 function play() {
-userPlay();
-computerPlay();
-let a = userPlay.choice ;
-let b = userPlay.choice;
 
-    if (a = b){
-        alert("yo elegi " + b + " Es un empate")
+let a = userPlay();
+let b = computerPlay();
 
+    if (a==b) {
+        alert("Yo elegi " + b + ". Es un empate");
     }
+    else if 
+     (((a== "rock") && (b == "scisor")) || ((a == "scisor") && (b == "paper")) || ((a=="paper") && (b == "rock"))){
+         alert("Yo elegi " + b + ". Ganaste!");
+     }
+     else {
+         alert("Yo elegi " + b + ". Perdiste");
+     }
 
 }
 
