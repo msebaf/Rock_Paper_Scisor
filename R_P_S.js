45378b55668053739322e89cd48d4788029024f5
msebaf <msebaf@hotmail.com>
2021-06-19 21:31:36 -0300

    LOgic finished, basic UI

--- R_P_S.js
+++ R_P_S.js
@@ -1,3 +1,33 @@
+
+let wons=0;
+let lost=0;
+const btnR = document.querySelector("#rock");
+btnR.addEventListener("click", function(e){
+    playRound("rock")
+    
+})
+
+const BtnP = document.querySelector("#paper");
+BtnP.addEventListener("click", function(e){
+    playRound("paper")
+    
+})
+
+const BtnS = document.querySelector("#scisors");
+BtnS.addEventListener("click", function(e){
+    playRound("scisor")
+    
+})
+
+
+const container = document.querySelector(".container");
+const divresult = document.createElement("div");
+container.appendChild(divresult);
+divresult.style.backgroundColor="pink";
+let results = document.createTextNode("resultados "+"ganados "+ wons+ " perdidos "+ lost);
+divresult.appendChild(results);
+
+
 //La computadora elige
 function computerPlay(){
     let computerChoise = "";
@@ -24,7 +54,7 @@ function computerPlay(){
 }
 
 // el usuario elige
-function userPlay () {
+/*function userPlay () {
     
     
     player_Selection = prompt ("Rock, Paper or Scisor").toLowerCase()
@@ -33,35 +63,61 @@ function userPlay () {
     return (player_Selection);
     
 }
+*/
 
+function playRound(eleccion) {
 
-function play() {
-
-let a = userPlay();
+let a = eleccion;
 let b = computerPlay();
 
     if (a==b) {
         alert("Yo elegi " + b + ". Es un empate");
-        
+        return console.log("empate")
     }
     else if 
      (((a== "rock") && (b == "scisor")) || ((a == "scisor") && (b == "paper")) || ((a=="paper") && (b == "rock"))){
+         wons++
+         results.textContent = "resultados "+"ganados "+ wons+ " perdidos "+ lost;
+         console.log("w"+wons);
+         if (wons < 5){
          alert("Yo elegi " + b + ". Ganaste!");
-         return(true);
+         }
+         else if (wons==5){
+            wons=0;
+            lost=0;
+            results.textContent = "resultados "+"ganados "+ wons+ " perdidos "+ lost;
+             alert("Ganaste el juego!")
+             
+         }
      }
      else {
+         
+         lost++
+         results.textContent = "resultados "+"ganados "+ wons+ " perdidos "+ lost;
+         console.log ("L"+lost)
+         if (lost < 5){
          alert("Yo elegi " + b + ". Perdiste");
-         return(false)
+         }
+         else if (lost == 5){
+            wons=0;
+            lost=0;
+            results.textContent = "resultados "+"ganados "+ wons+ " perdidos "+ lost;
+             alert("Perdiste el juego!")
+             
+         }
      }
+
 
 }
 
-function playRound(){
+
+
+/* function playFive(){
     
     let wons=0;
     let lost=0;
     for (i=0; i<5; i++){
-      let result = play();
+      let result = playRound();
       if (result==false) {
           lost++
           console.log("ganados "+wons+" perdidos "+lost);
@@ -83,6 +139,10 @@ function playRound(){
     }
     else{
         alert("Perdiste el juego")
-    }
-}
+    } 
+} */
+
+
+
+
 
